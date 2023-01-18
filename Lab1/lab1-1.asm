db73223418a02fcee9a5c62656ec3d5a6005ac42
Andrew SZETO <zkwokleung@gmail.com>
2023-01-19 05:30:55 +0800

    Update comments and experimental codes

--- Lab1/lab1-1.asm
+++ Lab1/lab1-1.asm
@@ -1,5 +1,8 @@
 .global _start
 
+#########################################
+#	1: define variables
+#########################################
 .data
 var1: .byte 15
 var2: .byte 19
@@ -7,6 +10,9 @@ newline: .ascii "\n"
 
 .text
 _start:
+#########################################
+#	2: print addresses of variables
+#########################################
 	# Print the address of var1
 	li a7, 1
 	la a0, var1
@@ -23,6 +29,9 @@ _start:
 	# New line
 	jal print_newline
 	
+#########################################
+#	3: Change the value of var1 and var2
+#########################################
 	# Increase var1 by 1
 	la t0, var1
 	lb t1, (t0)
@@ -33,46 +42,55 @@ _start:
 	lb t2, (t0)
 	slli t2, t2, 2
 	
-	# print var1
-	li a7, 1
-	mv a0, t1
-	ecall
+	# store resulting addition back to var1
+	sb t1, var1, t0
 	
-	# New line
-	jal print_newline
+	# store resulting multiplciation back to var2
+	sb t2, var2, t0
 	
-	# print var2
-	li a7, 1
-	mv a0, t2
-	ecall
+#########################################
+#	4: print var1 and var2
+#########################################
+	jal print_vars
 	
-	# New line
-	jal print_newline
-	
+#########################################
+#	5: Swap and print
+#########################################
 	# store resulting addition to var2
 	sb t1, var2, t0
 	
 	# store resulting multiplciation to var1
 	sb t2, var1, t0
 	
+	jal print_vars
 	
+	# Exit
+	li a7, 10
+	ecall
+	
+print_vars:
 	# Print the value of var1
 	li a7, 1
 	lb a0, var1
 	ecall
 	
 	# New line
-	jal print_newline
+	li a7, 4
+	la a0, newline
+	ecall
+
 	
 	# Print the value of var2
 	li a7, 1
 	lb a0, var2
 	ecall
 	
-	# Exit
-	li a7, 10
+	# New line
+	li a7, 4
+	la a0, newline
 	ecall
-	
+	ret
+
 print_newline:
 	li a7, 4
 	la a0, newline
