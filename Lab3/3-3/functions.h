7e6479100f714969487c1d47e355368451e76ea9
Andrew SZETO <zkwokleung@gmail.com>
2023-04-19 17:41:49 +0800

    [Lab3-3] Update runable codes

--- Lab3/3-3/functions.h
+++ Lab3/3-3/functions.h
@@ -8,12 +8,30 @@
 /*                                                             */
 /***************************************************************/
 
-
 #ifndef __FUNCTION_BLOCK_H__
 #define __FUNCTION_BLOCK_H__
 
+#include "util.h"
 
-#include "riscv-lc.h"
-
+unsigned int mask_val(int, int, int);
+int sext_unit(int, int);
+int rs2_mux(int, int, int);
+int mar_mux(int, int, int);
+int alu(int, int, int, int);
+int addr2_mux(int, int, int, int, int);
+int addr1_mux(int, int, int, int, int);
+int logic_shift_20_function_unit(int);
+int shift_function_unit(int, int, int, int);
+int pc_mux(int, int, int);
+int s_format_imm_gen_unit(int, int);
+int b_format_imm_gen_unit(int, int, int, int);
+int j_format_imm_gen_unit(int, int, int, int);
+int mdr_mux(int, int, int);
+int rs1_en(int, int);
+int rs2_en(int, int);
+int alu_shift_mux(int, int, int);
+int blockBMUX(int, int, int);
+int compare_function_unit(int, int, int, int);
+int datasize_mux(unsigned int, int, int);
 
 #endif
--- Lab3/3-3/riscv-lc.c
+++ Lab3/3-3/riscv-lc.c
@@ -8,18 +8,17 @@
 /*                                                             */
 /***************************************************************/
 
-
 #include "riscv-lc.h"
-
 
 /*
  * execute a cycle
  */
-void cycle() {
+void cycle()
+{
     /*
      * core steps
      */
-    eval_micro_sequencer();   
+    eval_micro_sequencer();
     cycle_memory();
     eval_bus_drivers();
     drive_bus();
@@ -30,95 +29,66 @@ void cycle() {
     CYCLE_COUNT++;
 }
 
-
-void eval_bus_drivers() {
+void eval_bus_drivers()
+{
     value_of_GatePC = 0;
     value_of_GateMAR = 0;
     value_of_GateMDR = 0;
     value_of_GateALUSHF = 0;
     value_of_GateRS2 = 0;
 
-    int value_of_MARMUX = 0,
-        value_of_alu,
-        value_of_shift_function_unit = 0;
+    int value_of_MARMUX = 0, value_of_alu, value_of_shift_function_unit = 0;
 
-    value_of_MARMUX = addr2_mux(
-        get_ADDR2MUX(CURRENT_LATCHES.MICROINSTRUCTION),
-        0,
-        sext_unit(mask_val(CURRENT_LATCHES.IR, 31, 20), 12),
-        sext_unit(
-            s_format_imm_gen_unit(
-                mask_val(CURRENT_LATCHES.IR, 11, 7),
-                mask_val(CURRENT_LATCHES.IR, 31, 25)
-            ),
-            12
-        ),
-        sext_unit(
-            j_format_imm_gen_unit(
-                mask_val(CURRENT_LATCHES.IR, 31, 31),
-                mask_val(CURRENT_LATCHES.IR, 30, 21),
-                mask_val(CURRENT_LATCHES.IR, 20, 20),
-                mask_val(CURRENT_LATCHES.IR, 19, 12)
-            ),
-            20
-        )
-    ) + addr1_mux(
-        get_ADDR1MUX(CURRENT_LATCHES.MICROINSTRUCTION),
-        0,
-        CURRENT_LATCHES.PC,
-        rs1_en(
-            get_RS1En(CURRENT_LATCHES.MICROINSTRUCTION),
-            CURRENT_LATCHES.REGS[mask_val(CURRENT_LATCHES.IR, 19, 15)]
-        ),
-        sext_unit(
-            b_format_imm_gen_unit(
-                mask_val(CURRENT_LATCHES.IR, 7, 7),
-                mask_val(CURRENT_LATCHES.IR, 11, 8),
-                mask_val(CURRENT_LATCHES.IR, 30, 25),
-                mask_val(CURRENT_LATCHES.IR, 31, 31)
-            ),
-            12
-        )
-    );
+    value_of_MARMUX =
+        addr2_mux(
+            get_ADDR2MUX(CURRENT_LATCHES.MICROINSTRUCTION), 0, sext_unit(mask_val(CURRENT_LATCHES.IR, 31, 20), 12),
+            sext_unit(s_format_imm_gen_unit(mask_val(CURRENT_LATCHES.IR, 11, 7), mask_val(CURRENT_LATCHES.IR, 31, 25)),
+                      12),
+            sext_unit(j_format_imm_gen_unit(mask_val(CURRENT_LATCHES.IR, 31, 31), mask_val(CURRENT_LATCHES.IR, 30, 21),
+                                            mask_val(CURRENT_LATCHES.IR, 20, 20), mask_val(CURRENT_LATCHES.IR, 19, 12)),
+                      20)) +
+        addr1_mux(
+            get_ADDR1MUX(CURRENT_LATCHES.MICROINSTRUCTION), 0, CURRENT_LATCHES.PC,
+            rs1_en(get_RS1En(CURRENT_LATCHES.MICROINSTRUCTION),
+                   CURRENT_LATCHES.REGS[mask_val(CURRENT_LATCHES.IR, 19, 15)]),
+            sext_unit(b_format_imm_gen_unit(mask_val(CURRENT_LATCHES.IR, 7, 7), mask_val(CURRENT_LATCHES.IR, 11, 8),
+                                            mask_val(CURRENT_LATCHES.IR, 30, 25), mask_val(CURRENT_LATCHES.IR, 31, 31)),
+                      12));
 
     /*
      *  Lab3-3 assignment
      */
     /* input of GateMAR */
-    // value_of_GateMAR = ?;
-    error("Lab3-3 assignment: value_of_GateMAR = ?;\n");
+    value_of_GateMAR = mar_mux(get_MARMUX(CURRENT_LATCHES.MICROINSTRUCTION), value_of_MARMUX,
+                               logic_shift_20_function_unit(mask_val(CURRENT_LATCHES.IR, 31, 12)));
 
     /* output of ALU */
     value_of_alu = alu(
-        mask_val(CURRENT_LATCHES.IR, 14, 12),
-        mask_val(CURRENT_LATCHES.IR, 31, 25),
-        rs1_en(
-            get_RS1En(CURRENT_LATCHES.MICROINSTRUCTION),
-            CURRENT_LATCHES.REGS[mask_val(CURRENT_LATCHES.IR, 19, 15)]
-        ),
-        rs2_mux(
-            get_RS2MUX(CURRENT_LATCHES.MICROINSTRUCTION),
-            rs2_en(
-                get_RS2En(CURRENT_LATCHES.MICROINSTRUCTION),
-                CURRENT_LATCHES.REGS[mask_val(CURRENT_LATCHES.IR, 24, 20)]
-            ),
-            sext_unit(mask_val(CURRENT_LATCHES.IR, 31, 20), 12)
-        )
-    );
+        mask_val(CURRENT_LATCHES.IR, 14, 12), mask_val(CURRENT_LATCHES.IR, 31, 25),
+        rs1_en(get_RS1En(CURRENT_LATCHES.MICROINSTRUCTION), CURRENT_LATCHES.REGS[mask_val(CURRENT_LATCHES.IR, 19, 15)]),
+        rs2_mux(get_RS2MUX(CURRENT_LATCHES.MICROINSTRUCTION),
+                rs2_en(get_RS2En(CURRENT_LATCHES.MICROINSTRUCTION),
+                       CURRENT_LATCHES.REGS[mask_val(CURRENT_LATCHES.IR, 24, 20)]),
+                sext_unit(mask_val(CURRENT_LATCHES.IR, 31, 20), 12)));
 
     /*
      *  Lab3-3 assignment
      */
     /* output of the shift function unit */
-    // value_of_shift_function_unit = ?;
-    error("Lab3-3 assignment: value_of_shift_function_unit = ?;\n");
+    value_of_shift_function_unit = shift_function_unit(
+        mask_val(CURRENT_LATCHES.IR, 14, 12), mask_val(CURRENT_LATCHES.IR, 31, 25),
+        rs1_en(CURRENT_LATCHES.REGS[mask_val(CURRENT_LATCHES.IR, 19, 15)], get_RS1En(CURRENT_LATCHES.MICROINSTRUCTION)),
+        rs2_mux(get_RS2MUX(CURRENT_LATCHES.MICROINSTRUCTION),
+                rs2_en(get_RS2En(CURRENT_LATCHES.MICROINSTRUCTION),
+                       CURRENT_LATCHES.REGS[mask_val(CURRENT_LATCHES.IR, 24, 20)]),
+                sext_unit(mask_val(CURRENT_LATCHES.IR, 31, 20), 12)));
 
     /*
      *  Lab3-3 assignment
      */
     /* input of GateALUSHF */
-    // value_of_GateALUSHF = ?;
-    error("Lab3-3 assignment: value_of_GateALUSHF = ?;\n");
+    value_of_GateALUSHF =
+        alu_shift_mux(mask_val(CURRENT_LATCHES.IR, 14, 12), value_of_alu, value_of_shift_function_unit);
 
     /* input of GatePC */
     value_of_GatePC = CURRENT_LATCHES.PC;
@@ -127,14 +97,15 @@ void eval_bus_drivers() {
      *  Lab3-3 assignment
      */
     /* input of GateRS2 */
-    error("Lab3-3 assignment: value_of_GateRS2 = ?;\n");
+    value_of_GateRS2 =
+        rs2_en(get_RS2En(CURRENT_LATCHES.MICROINSTRUCTION), CURRENT_LATCHES.REGS[mask_val(CURRENT_LATCHES.IR, 24, 20)]);
 
     /* input of GateMDR */
     value_of_GateMDR = CURRENT_LATCHES.MDR;
 }
 
-
-void drive_bus() {
+void drive_bus()
+{
     int _GateMAR = get_GateMAR(CURRENT_LATCHES.MICROINSTRUCTION);
     int _GateALUSHF = get_GateALUSHF(CURRENT_LATCHES.MICROINSTRUCTION);
     int _GatePC = get_GatePC(CURRENT_LATCHES.MICROINSTRUCTION);
@@ -144,31 +115,38 @@ void drive_bus() {
     /*
      *  Lab3-3 assignment
      */
-    switch ((_GateMDR << 4) + (_GateRS2 << 3) + (_GatePC << 2) + (_GateALUSHF << 1) + (_GateMAR)) {
-        case 0:
-            BUS = 0;
-            break;
-        case 1:
-            error("Lab3-3 assignment: when value = 1, BUS = ?;\n");
-        case 2:
-            error("Lab3-3 assignment: when value = 2, BUS = ?;\n");
-        case 4:
-            error("Lab3-3 assignment: when value = 4, BUS = ?;\n");
-        case 8:
-            error("Lab3-3 assignment: when value = 8, BUS = ?;\n");
-        case 16:
-            error("Lab3-3 assignment: when value = 16, BUS = ?;\n");
-        default:
-            BUS = 0;
-            warn("unknown gate drivers for BUS\n");
+    switch ((_GateMDR << 4) + (_GateRS2 << 3) + (_GatePC << 2) + (_GateALUSHF << 1) + (_GateMAR))
+    {
+    case 0:
+        BUS = 0;
+        break;
+    case 1:
+        // GateMAR is 1
+        BUS = value_of_GateMAR;
+        break;
+    case 2:
+        // GateALUSHF is 1
+        BUS = value_of_GateALUSHF;
+    case 4:
+        // GatePC is 1
+        BUS = value_of_GatePC;
+    case 8:
+        // GateRS2 is 1
+        BUS = value_of_GateRS2;
+    case 16:
+        // GateMDR is 1
+        BUS = value_of_GateMDR;
+    default:
+        BUS = 0;
+        warn("unknown gate drivers for BUS\n");
     }
 }
-
 
 /*
  * load the micro-code into the ROM control unit
  */
-void init_control_state(char *ucode_filename) {
+void init_control_state(char *ucode_filename)
+{
     FILE *ucode = open(ucode_filename, "r");
     int i, j, index;
     char line[200];
@@ -176,49 +154,43 @@ void init_control_state(char *ucode_filename) {
     info("load the micro: %s\n", ucode_filename);
 
     /* Read a line for each row in the control store */
-    for(i = 0; i < CONTROL_STATE_ROWS; i++) {
-        if (fscanf(ucode, "%[^\n]\n", line) == EOF) {
-            error(
-                "unexpected end of the file at line %d in the micro-code: %s\n",
-                i + 1, ucode_filename
-            );
+    for (i = 0; i < CONTROL_STATE_ROWS; i++)
+    {
+        if (fscanf(ucode, "%[^\n]\n", line) == EOF)
+        {
+            error("unexpected end of the file at line %d in the micro-code: %s\n", i + 1, ucode_filename);
         }
 
         /* Put in bits one at a time */
         index = 0;
-        for (j = 0; j < CONTROL_SIGNAL_BITS; j++) {
+        for (j = 0; j < CONTROL_SIGNAL_BITS; j++)
+        {
             /* Needs to find enough bits in line */
-            if (line[index] == '\0') {
-                error(
-                    "unexpected end of control bits at line %d in the micro-code: %s\n",
-                    i + 1, ucode_filename
-                );
+            if (line[index] == '\0')
+            {
+                error("unexpected end of control bits at line %d in the micro-code: %s\n", i + 1, ucode_filename);
             }
-            if (line[index] != '0' && line[index] != '1') {
-                error(
-                    "unknown control bit %d at line %d in the micro-code: %s\n",
-                    j + 1, i + 1, ucode_filename
-                );
+            if (line[index] != '0' && line[index] != '1')
+            {
+                error("unknown control bit %d at line %d in the micro-code: %s\n", j + 1, i + 1, ucode_filename);
             }
             /* set the bit in the ROM control unit */
-            CONTROL_STATE[i][j] = (line[index] == '0') ? 0:1;
+            CONTROL_STATE[i][j] = (line[index] == '0') ? 0 : 1;
             index++;
         }
         /* warn about extra bits in line */
-        if (line[index] != '\0') {
-            warn(
-                "extra bit(s) at line %d in the micro-code %s\n",
-                i + 1, ucode_filename
-            );
+        if (line[index] != '\0')
+        {
+            warn("extra bit(s) at line %d in the micro-code %s\n", i + 1, ucode_filename);
         }
     }
 }
 
-
 /*
  * load the binary program into the main memory
  */
-void load_program(char *program_filename) {
+void load_program(char *program_filename)
+{
     FILE *prog;
     int i, word, program_base;
 
@@ -229,13 +201,12 @@ void load_program(char *program_filename) {
     program_base = CODE_BASE_ADDR;
 
     i = 0;
-    while (fscanf(prog, "%x\n", &word) != EOF) {
+    while (fscanf(prog, "%x\n", &word) != EOF)
+    {
         /* make sure the program fit the memory */
-        if (program_base + i >= BYTES_IN_MEM) {
-            error(
-                "program file %s is too long to fit in memory at the address: %x\n",
-                program_filename, i
-            );
+        if (program_base + i >= BYTES_IN_MEM)
+        {
+            error("program file %s is too long to fit in memory at the address: %x\n", program_filename, i);
         }
 
         /*
@@ -255,47 +226,45 @@ void load_program(char *program_filename) {
     info("read %d bytes from the program into the memory.\n\n", i);
 }
 
-
 /*
  * zero out the memory array
  */
-void init_memory() {
+void init_memory()
+{
     int i;
 
     for (i = 0; i < BYTES_IN_MEM; i++)
         MEMORY[i] = 0;
 }
 
-
 /*
  * Load a micro-code and a binary file, and set up the initial state of the machine.
  */
-void initialize(char *ucode_filename, char *program_filename, int num_prog_files) {
+void initialize(char *ucode_filename, char *program_filename, int num_prog_files)
+{
     int i;
     init_control_state(ucode_filename);
 
     init_memory();
-    for (i = 0; i < num_prog_files; i++) {
+    for (i = 0; i < num_prog_files; i++)
+    {
         load_program(program_filename);
-        while(*program_filename++ != '\0');
+        while (*program_filename++ != '\0')
+            ;
     }
     CURRENT_LATCHES.STATE_NUMBER = INITIAL_STATE_NUMBER;
-    memcpy(
-        CURRENT_LATCHES.MICROINSTRUCTION,
-        CONTROL_STATE[INITIAL_STATE_NUMBER],
-        sizeof(int) * CONTROL_SIGNAL_BITS
-    );
+    memcpy(CURRENT_LATCHES.MICROINSTRUCTION, CONTROL_STATE[INITIAL_STATE_NUMBER], sizeof(int) * CONTROL_SIGNAL_BITS);
 
     NEXT_LATCHES = CURRENT_LATCHES;
 
     RUN_BIT = true;
 }
 
-
 /*
  * print the help menu
  */
-void help() {
+void help()
+{
     printf("--------------------- RISCV LC SIM Help ----------------------\n");
     printf("go             -  run a program till the end                  \n");
     printf("run n          -  execute a program for n instructions        \n");
@@ -305,49 +274,34 @@ void help() {
     printf("quit           -  exit the simulator                        \n\n");
 }
 
-
 /*
  * dump a word-aligned region of the main memory to the output file.
  */
-void mdump(FILE *dumpsim_file, int start, int stop) {
+void mdump(FILE *dumpsim_file, int start, int stop)
+{
     int address;
 
     printf("\nmemory content [0x%08x..0x%08x]:\n", start, stop);
     printf("-------------------------------------\n");
     for (address = start; address <= stop; address += 4)
-        printf(
-            "  0x%08x (%d) : 0x%02x%02x%02x%02x\n",
-            address,
-            address,
-            MEMORY[address + 3],
-            MEMORY[address + 2],
-            MEMORY[address + 1],
-            MEMORY[address]
-        );
+        printf("  0x%08x (%d) : 0x%02x%02x%02x%02x\n", address, address, MEMORY[address + 3], MEMORY[address + 2],
+               MEMORY[address + 1], MEMORY[address]);
     printf("\n");
 
     /* dump the memory contents into the dumpsim file */
     fprintf(dumpsim_file, "\nMemory content [0x%08x..0x%08x]:\n", start, stop);
     fprintf(dumpsim_file, "-------------------------------------\n");
     for (address = start; address <= stop; address += 4)
-        fprintf(
-            dumpsim_file,
-            " 0x%08x (%d) : 0x%02x%02x%02x%02x\n",
-            address,
-            address,
-            MEMORY[address + 3],
-            MEMORY[address + 2],
-            MEMORY[address + 1],
-            MEMORY[address]
-        );
+        fprintf(dumpsim_file, " 0x%08x (%d) : 0x%02x%02x%02x%02x\n", address, address, MEMORY[address + 3],
+                MEMORY[address + 2], MEMORY[address + 1], MEMORY[address]);
     fprintf(dumpsim_file, "\n");
 }
-
 
 /*
  * dump current registers and bus values to the output file
  */
-void rdump(FILE *dumpsim_file) {
+void rdump(FILE *dumpsim_file)
+{
     int k;
 
     printf("\ncurrent register/bus values:\n");
@@ -385,11 +339,11 @@ void rdump(FILE *dumpsim_file) {
     printf("\n");
 }
 
-
 /*
  * dump current signals to the output file
  */
-void sdump(FILE *dumpsim_file) {
+void sdump(FILE *dumpsim_file)
+{
     printf("signals: \n");
     printf("  IRD: %d\t", get_IRD(CURRENT_LATCHES.MICROINSTRUCTION));
     printf("  J: %d\n", get_J(CURRENT_LATCHES.MICROINSTRUCTION));
@@ -416,7 +370,7 @@ void sdump(FILE *dumpsim_file) {
     printf("  WE: %d\t", get_WE(CURRENT_LATCHES.MICROINSTRUCTION));
     printf("  RESET: %d\n", get_RESET(CURRENT_LATCHES.MICROINSTRUCTION));
 
-    for(int i = 0; i < CONTROL_SIGNAL_BITS; i++)
+    for (int i = 0; i < CONTROL_SIGNAL_BITS; i++)
         fprintf(dumpsim_file, "%d", CURRENT_LATCHES.MICROINSTRUCTION[i]);
     fprintf(dumpsim_file, "\n");
     fprintf(dumpsim_file, "Signals: \n");
@@ -446,11 +400,11 @@ void sdump(FILE *dumpsim_file) {
     fprintf(dumpsim_file, "  RESET: %d\n", get_RESET(CURRENT_LATCHES.MICROINSTRUCTION));
 }
 
-
 /*
  * simulate the RISCV LC with benchmarks until it is halted
  */
-void go() {
+void go()
+{
     if (RUN_BIT == false)
         error("RISCV LC cannot simulate, and the simulator is halted.\n\n");
 
@@ -461,19 +415,21 @@ void go() {
     info("RISCV LC is halted.\n\n");
 }
 
-
 /*
  * simulate the RISCV LC with benchmarks for n cycles
  */
-void run(int num_cycles) {
+void run(int num_cycles)
+{
     int i;
 
     if (RUN_BIT == false)
         error("RISCV LC cannot simulate, and the simulator is halted.\n\n");
 
     info("simulating for %d cycles...\n\n", num_cycles);
-    for (i = 0; i < num_cycles; i++) {
-        if (CURRENT_LATCHES.PC == TRAPVEC_BASE_ADDR) {
+    for (i = 0; i < num_cycles; i++)
+    {
+        if (CURRENT_LATCHES.PC == TRAPVEC_BASE_ADDR)
+        {
             RUN_BIT = false;
             info("RISCV LC is halted.\n\n");
             break;
@@ -482,11 +438,11 @@ void run(int num_cycles) {
     }
 }
 
-
 /*
  * read a command from the standard input.
  */
-void get_command(FILE *dumpsim_file) {
+void get_command(FILE *dumpsim_file)
+{
     char buffer[20];
     int start, stop, cycles;
 
@@ -495,50 +451,53 @@ void get_command(FILE *dumpsim_file) {
     scanf("%s", buffer);
     printf("\n");
 
-    switch(buffer[0]) {
-        case 'G':
-        case 'g':
-            go();
-            break;
-        case 'M':
-        case 'm':
-            scanf("%i %i", &start, &stop);
-            mdump(dumpsim_file, start, stop);
-            break;
-        case '?':
-        case 'h':
-        case 'H':
-            help();
-            break;
-        case 'Q':
-        case 'q':
-            printf("bye.\n");
-            exit(EXIT_SUCCESS);
-        case 'R':
-        case 'r':
-            if (buffer[1] == 'd' || buffer[1] == 'D')
-                rdump(dumpsim_file);
-            else {
-                scanf("%d", &cycles);
-                run(cycles);
-            }
-            break;
-        case 'S':
-        case 's':
-            sdump(dumpsim_file);
-            break;
-        default:
-            printf("invalid command\n");
-            break;
+    switch (buffer[0])
+    {
+    case 'G':
+    case 'g':
+        go();
+        break;
+    case 'M':
+    case 'm':
+        scanf("%i %i", &start, &stop);
+        mdump(dumpsim_file, start, stop);
+        break;
+    case '?':
+    case 'h':
+    case 'H':
+        help();
+        break;
+    case 'Q':
+    case 'q':
+        printf("bye.\n");
+        exit(EXIT_SUCCESS);
+    case 'R':
+    case 'r':
+        if (buffer[1] == 'd' || buffer[1] == 'D')
+            rdump(dumpsim_file);
+        else
+        {
+            scanf("%d", &cycles);
+            run(cycles);
+        }
+        break;
+    case 'S':
+    case 's':
+        sdump(dumpsim_file);
+        break;
+    default:
+        printf("invalid command\n");
+        break;
     }
 }
 
-
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     FILE *dumpsim_file = open("dumpsim", "w");
 
     /* Error Checking */
-    if (argc != 3) {
+    if (argc != 3)
+    {
         error("Usage: %s <ucode> <*.bin>\n", argv[0]);
     }
 
